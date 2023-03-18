43cada9ac959760c7cb273de0155aedca3cd94c3
happysalada <raphael@megzari.com>
2023-03-18 18:08:20 -0400

    deploy: test fix for global not defined fix

--- src/lib/ConnectWalletModal.svelte
+++ src/lib/ConnectWalletModal.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  window.global ||= window;
   import {
     defaultEvmStores,
     connected,
--- src/routes/inbox/+page.svelte
+++ src/routes/inbox/+page.svelte
@@ -1,17 +1,35 @@
 <script lang="ts">
   window.global ||= window;
+  import type { Conversation } from "@xmtp/xmtp-js";
   import { xmtpClient } from '$lib/stores'
   import { goto } from "$app/navigation"
   import { onMount } from "svelte";
+  import { signerAddress } from "ethers-svelte";
+
+  // TODO find a way to cache conversations from different wallets
+  // import { conversations } from "$lib/stores"
+
+  let conversations: Conversation[] = [];
 
   onMount(async () => {
     if ($xmtpClient) {
-      const conversations = await $xmtpClient.conversations.list()
-      console.log(conversations)
+      conversations = await $xmtpClient.conversations.list()
     } else {
       goto("/")
     }
   });
+
+  function formatEthAddress(address) {
+    if (typeof address !== 'string' || address.length < 10) {
+      throw new Error('Invalid Ethereum address');
+    }
+
+    const firstFour = address.slice(0, 6);
+    const lastFour = address.slice(-4);
+
+    return `${firstFour}...${lastFour}`;
+  }
+
 </script>
 <section
   class="flex flex-col justify-center antialiased bg-gray-50 text-gray-600 min-h-screen p-4"
@@ -24,26 +42,24 @@
         <div class="flex justify-between items-center mb-3">
           <!-- Image + name -->
           <div class="flex items-center">
+            <!-- TODO at some point generate a random image ?
             <a class="inline-flex items-start mr-3" href="#0">
               <img
                 class="rounded-full"
-                src="https://res.cloudinary.com/dc6deairt/image/upload/v1638102932/user-48-01_nugblk.jpg"
+                src=""
                 width="48"
                 height="48"
-                alt="Lauren Marsano"
+                alt={$signerAddress}
               />
             </a>
+            -->
             <div class="pr-1">
               <a
                 class="inline-flex text-gray-800 hover:text-gray-900"
                 href="#0"
               >
-                <h2 class="text-xl leading-snug font-bold">Lauren Marsano</h2>
+                <h2 class="text-xl leading-snug font-bold">{formatEthAddress($signerAddress)}</h2>
               </a>
-              <a
-                class="block text-sm font-medium hover:text-indigo-500"
-                href="#0">@lauren.mars</a
-              >
             </div>
           </div>
           <!-- Settings button -->
@@ -61,33 +77,6 @@
           </div>
         </div>
         <!-- Meta -->
-        <div class="flex flex-wrap justify-center sm:justify-start space-x-4">
-          <div class="flex items-center">
-            <svg
-              class="w-4 h-4 fill-current flex-shrink-0 text-gray-400"
-              viewBox="0 0 16 16"
-            >
-              <path
-                d="M8 8.992a2 2 0 1 1-.002-3.998A2 2 0 0 1 8 8.992Zm-.7 6.694c-.1-.1-4.2-3.696-4.2-3.796C1.7 10.69 1 8.892 1 6.994 1 3.097 4.1 0 8 0s7 3.097 7 6.994c0 1.898-.7 3.697-2.1 4.996-.1.1-4.1 3.696-4.2 3.796-.4.3-1 .3-1.4-.1Zm-2.7-4.995L8 13.688l3.4-2.997c1-1 1.6-2.198 1.6-3.597 0-2.798-2.2-4.996-5-4.996S3 4.196 3 6.994c0 1.399.6 2.698 1.6 3.697 0-.1 0-.1 0 0Z"
-              />
-            </svg>
-            <span class="text-sm whitespace-nowrap ml-2">Milan, IT</span>
-          </div>
-          <div class="flex items-center">
-            <svg
-              class="w-4 h-4 fill-current flex-shrink-0 text-gray-400"
-              viewBox="0 0 16 16"
-            >
-              <path
-                d="M11 0c1.3 0 2.6.5 3.5 1.5 1 .9 1.5 2.2 1.5 3.5 0 1.3-.5 2.6-1.4 3.5l-1.2 1.2c-.2.2-.5.3-.7.3-.2 0-.5-.1-.7-.3-.4-.4-.4-1 0-1.4l1.1-1.2c.6-.5.9-1.3.9-2.1s-.3-1.6-.9-2.2C12 1.7 10 1.7 8.9 2.8L7.7 4c-.4.4-1 .4-1.4 0-.4-.4-.4-1 0-1.4l1.2-1.1C8.4.5 9.7 0 11 0ZM8.3 12c.4-.4 1-.5 1.4-.1.4.4.4 1 0 1.4l-1.2 1.2C7.6 15.5 6.3 16 5 16c-1.3 0-2.6-.5-3.5-1.5C.5 13.6 0 12.3 0 11c0-1.3.5-2.6 1.5-3.5l1.1-1.2c.4-.4 1-.4 1.4 0 .4.4.4 1 0 1.4L2.9 8.9c-.6.5-.9 1.3-.9 2.1s.3 1.6.9 2.2c1.1 1.1 3.1 1.1 4.2 0L8.3 12Zm1.1-6.8c.4-.4 1-.4 1.4 0 .4.4.4 1 0 1.4l-4.2 4.2c-.2.2-.5.3-.7.3-.2 0-.5-.1-.7-.3-.4-.4-.4-1 0-1.4l4.2-4.2Z"
-              />
-            </svg>
-            <a
-              class="text-sm font-medium whitespace-nowrap text-indigo-500 hover:text-indigo-600 ml-2"
-              href="#0">carolinmcneail.com</a
-            >
-          </div>
-        </div>
       </header>
       <!-- Card body -->
       <div class="py-3 px-5">
@@ -97,105 +86,27 @@
         <!-- Chat list -->
         <div class="divide-y divide-gray-200">
           <!-- User -->
+          {#each conversations as {peerAddress, createdAt}}
           <button
             class="w-full text-left py-2 focus:outline-none focus-visible:bg-indigo-50"
           >
             <div class="flex items-center">
               <img
                 class="rounded-full items-start flex-shrink-0 mr-3"
-                src="https://res.cloudinary.com/dc6deairt/image/upload/v1638102932/user-32-01_pfck4u.jpg"
+                src=""
                 width="32"
                 height="32"
                 alt="Marie Zulfikar"
               />
               <div>
                 <h4 class="text-sm font-semibold text-gray-900">
-                  Marie Zulfikar
+                  {peerAddress}
                 </h4>
                 <div class="text-[13px]">The video chat ended · 2hrs</div>
               </div>
             </div>
           </button>
-          <!-- User -->
-          <button
-            class="w-full text-left py-2 focus:outline-none focus-visible:bg-indigo-50"
-          >
-            <div class="flex items-center">
-              <img
-                class="rounded-full items-start flex-shrink-0 mr-3"
-                src="https://res.cloudinary.com/dc6deairt/image/upload/v1638102932/user-32-02_vll8uv.jpg"
-                width="32"
-                height="32"
-                alt="Nhu Cassel"
-              />
-              <div>
-                <h4 class="text-sm font-semibold text-gray-900">Nhu Cassel</h4>
-                <div class="text-[13px]">Hello Lauren 👋, · 24 Mar</div>
-              </div>
-            </div>
-          </button>
-          <!-- User -->
-          <button
-            class="w-full text-left py-2 focus:outline-none focus-visible:bg-indigo-50"
-          >
-            <div class="flex items-center">
-              <img
-                class="rounded-full items-start flex-shrink-0 mr-3"
-                src="https://res.cloudinary.com/dc6deairt/image/upload/v1638102932/user-32-03_uzwykl.jpg"
-                width="32"
-                height="32"
-                alt="Patrick Friedman"
-              />
-              <div>
-                <h4 class="text-sm font-semibold text-gray-900">
-                  Patrick Friedman
-                </h4>
-                <div class="text-[13px]">Yes, you’re right but… · 14 Mar</div>
-              </div>
-            </div>
-          </button>
-          <!-- User -->
-          <button
-            class="w-full text-left py-2 focus:outline-none focus-visible:bg-indigo-50"
-          >
-            <div class="flex items-center">
-              <img
-                class="rounded-full items-start flex-shrink-0 mr-3"
-                src="https://res.cloudinary.com/dc6deairt/image/upload/v1638102932/user-32-04_ttlftd.jpg"
-                width="32"
-                height="32"
-                alt="Byrne McKenzie"
-              />
-              <div>
-                <h4 class="text-sm font-semibold text-gray-900">
-                  Byrne McKenzie
-                </h4>
-                <div class="text-[13px]">
-                  Hey Lauren ✨, first of all… · 14 Mar
-                </div>
-              </div>
-            </div>
-          </button>
-          <!-- User -->
-          <button
-            class="w-full text-left py-2 focus:outline-none focus-visible:bg-indigo-50"
-          >
-            <div class="flex items-center">
-              <img
-                class="rounded-full items-start flex-shrink-0 mr-3"
-                src="https://res.cloudinary.com/dc6deairt/image/upload/v1638102932/user-32-05_bktgmb.jpg"
-                width="32"
-                height="32"
-                alt="Scott Micheal"
-              />
-              <div>
-                <h4 class="text-sm font-semibold text-gray-900">
-                  Scott Micheal
-                </h4>
-                <div class="text-[13px]">No way 🤙! · 11 Mar</div>
-              </div>
-            </div>
-          </button>
+          {/each}
         </div>
       </div>
       <!-- Bottom right button -->
